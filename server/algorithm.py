5a670608ec7d97a81c054815ae4313586b7ae3df
Dongoug Kim <dougouk@gmail.com>
2018-04-23 22:30:08 -0600

    Updated app and algorithm

--- server/algorithm.py
+++ server/algorithm.py
@@ -31,9 +31,9 @@ def edge_d(a, b):
         graph.append([0] * m)
     
     # Call helper with graph
-    return helper(a, b, graph)
+    return edit_distance_helper(a, b, graph)
 
-def helper(a, b, graph):
+def edit_distance_helper(a, b, graph):
     
     # Solve smallest subproblem 
     n = len(a) + 1
@@ -52,7 +52,7 @@ def helper(a, b, graph):
             graph[i][j] = min(graph[i-1][j] + 1, graph[i][j-1] + 1, graph[i-1][j-1] + diff)
     return graph
     
-edge_d('exponential', 'polynomial')
+# edge_d('exponential', 'polynomial')
 
 # edge_d('Calgary', '')
 # edge_d('Snow', 'Know')
@@ -61,10 +61,10 @@ def lat_long_d(_lat_a, _long_a, _lat_b, _long_b):
     '''
     Returns distance between two points in km
     '''
-    lat_a = math.radians(_lat_a)
-    lat_b = math.radians(_lat_b)
-    long_a = math.radians(_long_a)
-    long_b = math.radians(_long_b)
+    lat_a = math.radians(float(_lat_a))
+    lat_b = math.radians(float(_lat_b))
+    long_a = math.radians(float(_long_a))
+    long_b = math.radians(float(_long_b))
     
     # approximate radius of earth in km
     R = 6373.0
@@ -77,51 +77,85 @@ def lat_long_d(_lat_a, _long_a, _lat_b, _long_b):
     distance = R * c
     return distance
 
-def calc_confidence_level(city):
+def calc_confidence_level(city, query_location):
     '''
     Input: City object variable
     Calculates and stores the confidence level of the city in-place (city variable is updated)
     
     Confidence level is calculated as follows:
     
-    -\-\-\-\-\-\
+    -\-\-\-\-\-
+    80% weight on edit distance
+    20% weight on physical proximity
+
+    If latitude and longitude are not provided, then 100% weight on edit distance
+
+    Edit distance score calculation
+    Score = I / (I + D^2)
+        I: Inertia
+        D: Edit Distance
+    Explanation
+        When Edit distance is 0 (names are an exact match), the score is a 1.
+        A non-linear decrease in score along with edit distance (Yoronto is 
+        much more closely matched with Toronto than Yoro).
+        
+    Physical distance score calculation
+    Score = 1 / (1 + physical_d / 100)
+        physical_d: physical distance in km
+    Explanation
+        Assumed that the objective of the data is for Canadian cities. So the 
+        physical distance between cities are only significant when in 100s of kms.
+        For example, if 2 cities are within 50 km of each other, their score shouldn't differ too much.
     
     Output: Calculate confidence level
     '''
     
-    INERTIA = 10
+    INERTIA = len(query_location) * 2
     confidence_level = 0.0
     if city.physical_distance:
-        confidence_level = 0.8 * (INERTIA / (INERTIA + city.edit_distance**2)) + 0.2 * ( 1 / (1 + city.physical_distance / 100))
+        confidence_level = 0.8 * (INERTIA / (INERTIA + city.edit_distance**2)) + 0.2 * ( 1 / (1 + city.physical_distance / 200))
     else:
         confidence_level = (INERTIA / (INERTIA + city.edit_distance**2))
     city.confidence_level = confidence_level
     return city.confidence_level
-    
-THRESHOLD = 3
 
+EDIT_DISTANCE_THRESHOLD = 3
 def search(query, lat=None, long=None):
+    '''
+    Inputs
+        query: name of city
+        lat: optional latitude parameter
+        longitude: optional longitude parameter
+    Output
+        Array of cities that may be good results based on the input parameters
+    '''
+    con = None
+    con = connect(database= 'coveo_cities', user='postgres', host = 'localhost', password='password')
+    cur = con.cursor()
+    # cur.execute('SELECT * FROM cities')
+    cur.execute('SELECT * FROM cities WHERE LOWER(cities.name) LIKE \'%' + query.lower() + '%\'')
+    rows = cur.fetchall()
+    
+    similar_cities = []
+    for row in rows:
+        confidence_graph = edge_d(query.lower(), row[1].lower())
+        if query.lower() in row[1].lower() or confidence_graph[-1][-1] < EDIT_DISTANCE_THRESHOLD:
+            city = City(row[1], row[4], row[5])
+            city.edit_distance = confidence_graph[-1][-1]
+            if lat and long:
+                city.physical_distance = lat_long_d(lat, long, row[4], row[5])
+
+            calc_confidence_level(city, query)
+            similar_cities.append(city)
+    cur.close()
+    con.close()
+    return similar_cities
+
+
+def rows_in_database():
     con = None
     con = connect(database= 'coveo_cities', user='postgres', host = 'localhost', password='password')
     cur = con.cursor()
     cur.execute('SELECT * FROM cities')
     rows = cur.fetchall()
-    
-    similar_cities = []
-    for row in rows:
-        confidence_graph = edge_d(query, row[1])
-        if confidence_graph[-1][-1] < THRESHOLD:
-            city = City(row[1], row[4], row[5])
-            city.edit_distance = confidence_graph[-1][-1]
-            if lat and long:
-                city.physical_distance = lat_long_d(lat, long, row[4], row[5])
-            
-            calc_confidence_level(city)
-            similar_cities.append(city)
-            
-
-    cur.close()
-    con.close()
-    return similar_cities
-    
-search('Toronot')
+    return len(rows)
--- server/app.py
+++ server/app.py
@@ -13,14 +13,11 @@ import json
 from flask import Flask, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
-# Set up logger, redis, app
+# Set up logger and flask app
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-r = redis.Redis(host='redis', decode_responses=True)
-
-# os.makedirs('data', exist_ok=True)
-# ALLOWED_EXTENSIONS = set(['wav'])
+# r = redis.Redis(host='redis', decode_responses=True)
 
 app = Flask(__name__)
 
@@ -29,27 +26,56 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/readall')
+def read_all():
+    result = dict()
+    result['rows'] = algorithm.goThroughDatabase()
+    return json.dumps(result)
+
 @app.route('/suggestions', methods=['GET'])
 def upload_wav():
     # Get query parameters
     query_location = request.args.get('q')
-    query_lat = request.args.get('latitude')
-    query_long = request.args.get('longitude')
+    response = dict()
 
-    logger.info(f'received {query_location} query')
+    if query_location is None:
+        logger.error('Location query is None')        
+        response['error'] = 'Location query must not be empty'
+        return json.dumps(response)
+    else:
+        query_lat = request.args.get('latitude')
+        query_long = request.args.get('longitude')
 
-    if query_lat:
-        logger.info(f'Additional location info: [{query_lat}:{query_long}]')
+        logger.info(f'Received {query_location} query')
 
-    results = algorithm.search('Yoronto', 41.43979137, -88.1234974)
-    sorted_results = sorted(results, key=lambda x: x.confidence_level, reverse=True)
-    sorted_results
+        if query_lat:
+            logger.info(f'Additional location info: [{query_lat}:{query_long}]')
 
-    result = {
-        'q': query_location,
-        'lat': query_lat,
-        'lng': query_long,
-        'results': [result.toJSON() for result in sorted_results]
-    }
+        results = algorithm.search(query_location, query_lat, query_long)
+        sorted_results = sorted(results, key=lambda x: x.confidence_level, reverse=True)
+        sorted_results
+
+        logger.info(f'Results: {sorted_results}')
     
-    return json.dumps(result)
+
+        '''
+        TODO
+        Wrap up the results into a json response.
+        O(n) operation. Is there a better way to encode it to a json response?
+        '''
+        response['suggestions'] = [ 
+            { 
+                'name': result.name,
+                'latitude': result.lat,
+                'longitude': result.long,
+                'score': result.confidence_level
+            } for result in sorted_results 
+        ]
+        result = {
+            'q': query_location,
+            'lat': query_lat,
+            'lng': query_long,
+            'results': [result.toJSON() for result in sorted_results]
+        }
+        
+        return json.dumps(response)
